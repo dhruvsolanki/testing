f83273368938fc7e45a217e7b5039860821dcdb3
dhruvsolanki <52849722+dhruvsolanki@users.noreply.github.com>
2024-02-08 13:21:56 +0530

    Add files via upload
    Adding java code for test

---
+++ src/main/java/com/goit/app/App.java
@@ -0,0 +1,19 @@
+package com.goit.app;
+
+import com.google.gson.Gson;
+
+/**
+ * Print name and surname in JSON
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        User user = new User();
+        user.setName("Vadim");
+        user.setSurname("Batyashov");
+        String json = new Gson().toJson(user);
+        System.out.println(json);
+    }
+}
---
+++ src/main/java/com/goit/app/User.java
@@ -0,0 +1,9 @@
+package com.goit.app;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String name;
+    private String surname;
+}
